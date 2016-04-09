e8b823be4d012ecb8d8eb3e2e09cd552d55ca7b5
Nebasuy Vadym <nebasuyvv@gmail.com>
2016-04-09 23:12:33 +0300

    test whith add email

---
+++ dist/send.php
@@ -0,0 +1,19 @@
+<?
+if((isset($_POST['wname'])&&$_POST['wname']!="")&&(isset($_POST['wemail'])&&$_POST['wemail']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'nebasuyvv@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Обратный звонок'; //Загаловок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['wname'].'</p>
+                        <p>E-mail: '.$_POST['wemail'].'</p>                        
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+?>
