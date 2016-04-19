c2699bc2682d5187ed061d8bfeabec1e88eed864
Nebasuy Vadym <nebasuyvv@gmail.com>
2016-04-19 16:25:07 +0300

    removed is not used

---
+++ fancoc/dist/js/social.js
@@ -0,0 +1,7 @@
+<script type="text/javascript" src="//vk.com/js/api/openapi.js?116"></script>
+
+<!-- VK Widget -->
+<div id="vk_groups"></div>
+<script type="text/javascript">
+VK.Widgets.Group("vk_groups", {mode: 2, width: "250", height: "300"}, 64814185);
+</script>
